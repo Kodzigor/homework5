5bacf66f32a15b823f8cf611ff484a5bdc884f7f
Igor Kodzhebash <igorkodzebash@gmail.com>
2022-04-07 13:20:48 +0300

    fix: add default to flaf argument, refatored code

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-const padString = (name, length, symbol, flag) => {
+const padString = (name, length, symbol, flag = true) => {
     let resString = '';
-    if(name.length < length && flag === true) {
-        for(let i = 0; i < length - name.length; i++) {
+
+    if (name.length < length && flag) {   
+        for (let i = 0; i < length - name.length; i++) {
             resString+=symbol;
-        } resString+=name;
-           
-    } else if(flag === false) {
+        } 
         resString+=name;
-        for(let i = 0; i < length - name.length; i++) {
+           
+    } else if (flag === false) {
+        resString+=name;
+
+        for (let i = 0; i < length - name.length; i++) {
             resString+=symbol;
         }
     }  
     return resString;
 };
 
-let result = padString("Ivan", 10, '?', false);
+let result = padString("Mikhael", 7, '?', false);
 console.log(result);
